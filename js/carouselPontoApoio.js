dadef253073bd2c9ac0a9432f8a56bd348c24fdc
Wellington <wellingtonoficial00@gmail.com>
2022-05-10 16:51:47 -0300

    90% da pagina ponto de apoio

---
+++ js/carouselPontoApoio.js
@@ -0,0 +1,19 @@
+
+
+
+const slider = document.querySelector('.slider');
+
+const indicatorParent = document.querySelector('.control ul'); 
+const indicators = document.querySelectorAll('.control li');
+index = 0;
+
+indicators.forEach((indicator, i) => {
+  indicator.addEventListener('click', () => {
+    document.querySelector('.control .selected').classList.remove('selected');
+    indicator.classList.add('selected');
+    slider.style.transform = 'translateX(' + (i) * -25 + '%)';  
+    index = i;
+    
+  });
+});
+
