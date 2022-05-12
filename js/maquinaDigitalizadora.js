93cddaec4ec6413a73811057dbbdcbfed38a2586
Wellington <wellingtonoficial00@gmail.com>
2022-05-11 23:06:45 -0300

    pagina contato finalizada

--- js/maquinaDigitalizadora.js
+++ js/maquinaDigitalizadora.js
@@ -1,17 +1,41 @@
 
-function typeWriter(elemento){
+
+
+  // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+  // function typeWriter(elemento) {
+  //   const textoArray = elemento.innerHTML.split('');
+  //   elemento.innerHTML = '';
+  //   for(let i = 0; i < textoArray.length; i++) {
+  //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+  //   }
+  // }
+
+
+
+function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(() =>{
-            elemento.innerHTML += letra
-        }, 40 * i);
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
     });
-    setInterval(()=>{
-        typeWriter(elemento)
-    },4000);
-}
+    // setInterval(()=>{
+    //     typeWriter(elemento)
+    // },4000);
+  }
+  const text = document.getElementById('textDigit');
+  typeWriter(text);
 
 
-const text =  document.getElementById('writing');
-typeWriter(text);
+
+  function typeWriter2(elemento) {
+    const textoArray2 = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray2.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+    // setInterval(()=>{
+    //     typeWriter(elemento)
+    // },4000);
+  }
+  const text2 = document.getElementById('textDigit2');
+  typeWriter2(text2);
