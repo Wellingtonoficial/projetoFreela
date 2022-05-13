0399395fd8547286feb02036cfb18a6d73a713d3
Wellington <wellingtonoficial00@gmail.com>
2022-05-12 23:09:34 -0300

    colocando as informações do cliente

---
+++ js/vidMake.js
@@ -0,0 +1,22 @@
+let btn_b = document.getElementById("videoMakeAtivoBtn")
+
+
+btn_b.addEventListener("click", function(){
+	let container_b = document.getElementById("videoMakeAtivo")
+
+	container_b.classList.toggle("vidmakeActiv")//se tem tira, se não tem coloca (classe)
+
+	let video = document.querySelector('video')
+	setTimeout(()=>{
+		video.play()
+	},2000)
+})
+
+let btnExluir = document.getElementById("btnExcluir")
+btnExluir.addEventListener("click", function(){
+	let container2 = document.getElementById("videoMakeAtivo")
+
+	container2.classList.toggle("vidmakeActiv")//se tem tira, se não tem coloca (classe)
+	
+})
+
